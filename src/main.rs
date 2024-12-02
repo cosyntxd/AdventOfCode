9cf2e63721b4ceb541f94db278caf5561b9de3ea
cosyntxd <110003521+cosyntxd@users.noreply.github.com>
2024-12-01 21:30:01 -0800

    not bad

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ mod solutions;
 fn main() {
     let start = Instant::now();
     println!("{:?}", day01::run());
+    println!("{:?}", day02::run());
+
     let duration = start.elapsed();
     println!("Done! 🎉 -- Elapsed time: {:?}", duration);
 
