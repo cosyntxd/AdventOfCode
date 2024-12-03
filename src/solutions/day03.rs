b37d1323db4ebc843ff69de715fdc315e42bbb57
cosyntxd <110003521+cosyntxd@users.noreply.github.com>
2024-12-02 22:01:46 -0800

    regex :(
    rust was the wrong language

---
+++ src/solutions/day03.rs
@@ -0,0 +1,35 @@
+use regex::Regex;
+
+fn mul(n: &str) -> u32 {
+    let str = n.replace("mul(", "").replace(")", "");
+    let (a,b) = str.split_at(str.find(',').unwrap());
+    return a.parse::<u32>().unwrap() * b.replace(",", "").parse::<u32>().unwrap();
+}
+
+pub fn run() -> (u32, u32) {
+    let input = std::fs::read_to_string("data/day03.txt").unwrap().trim().replace('\r', "");
+
+    let pattern = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+
+    let mut total_sum = 0;
+    for caps in pattern.find_iter(&input) {
+        total_sum += mul(caps.as_str());
+    }
+
+    let pattern2 = Regex::new(r"mul\(\d+,\d+\)|do(n't)?\(\)").unwrap();
+    let mut exec = true;
+    let mut total_2 = 0;
+    for caps in pattern2.find_iter(&input) {
+        match caps.as_str() {
+            "do()" => {exec = true},
+            "don't()" => {exec = false},
+            _ => {
+                if exec {
+                    total_2 += mul(caps.as_str());
+                }
+            }   
+        }
+    }
+
+    (total_sum as u32, total_2 as u32)
+}
