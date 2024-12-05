283508c4c0c7ac325186d8238b7c2aafabd8c89e
cosyntxd <110003521+cosyntxd@users.noreply.github.com>
2024-12-04 21:51:18 -0800

    i solved it i guess

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,11 @@ mod solutions;
 
 fn main() {
     let start = Instant::now();
-    println!("{:?}", day01::run());
-    println!("{:?}", day02::run());
-    println!("{:?}", day03::run());
-    println!("{:?}", day04::run());
+    // println!("{:?}", day01::run());
+    // println!("{:?}", day02::run());
+    // println!("{:?}", day03::run());
+    // println!("{:?}", day04::run());
+    println!("{:?}", day05::run());
     let duration = start.elapsed();
     println!("Done! 🎉 -- Elapsed time: {:?}", duration);
 
---
+++ src/solutions/day05.rs
@@ -0,0 +1,76 @@
+use itertools::Itertools;
+
+pub fn run() -> (i32, i32) {
+    let input = std::fs::read_to_string("data/day05.txt").unwrap().trim().replace('\r', "");
+    let input_iter = input.lines().collect::<Vec<&str>>();
+    let mut orderings = vec![];
+    for line in &input_iter {
+        if line.trim().len() == 0 {
+            break
+        };
+        let parsed = line.trim().split("|").map(|s| s.parse::<i32>().unwrap()).collect::<Vec<i32>>();
+        orderings.push((parsed[0], parsed[1]));
+    }
+    let mut sum = 0;
+    let mut sum2 = 0;
+    let mut bads = vec![];
+
+    for line in input_iter.iter().skip(orderings.len() +1) {
+        let mut isgood = true;
+
+        let nums = line.trim().split(',').map(|s| s.parse::<i32>().unwrap()).collect::<Vec<i32>>();
+        
+
+        for &(a, b) in &orderings {
+            let index1 = nums.iter().position(|&x| x == a);
+            let index2 = nums.iter().position(|&x| x == b);
+
+            if let (Some(i1), Some(i2)) = (index1, index2) {
+                if i1 > i2 {
+                    isgood = false;
+                    break;
+                }
+            }
+        }
+
+        if isgood {
+            sum += nums[nums.len() / 2];
+        } else {
+            bads.push(nums.clone());
+        }
+    }
+
+    for mut trial in bads {
+        while !(is_sorted(&trial, &orderings)) {
+            trial.sort_by(|a, b| {
+                if orderings.iter().any(|&(x, y)| x == *a && y == *b) {
+                    std::cmp::Ordering::Less
+                } else if orderings.iter().any(|&(x, y)| x == *b && y == *a) {
+                    std::cmp::Ordering::Greater
+                } else {
+                    std::cmp::Ordering::Equal
+                }
+            });
+        }
+
+        if is_sorted(&trial, &orderings) {
+            sum2 += trial[trial.len() / 2];
+            continue
+        }
+    }
+    (sum as i32, sum2)
+}
+
+fn is_sorted(nums: &Vec<i32>, orderings: &Vec<(i32, i32)>) -> bool {
+    for &(a, b) in orderings {
+        let index1 = nums.iter().position(|&x| x == a);
+        let index2 = nums.iter().position(|&x| x == b);
+
+        if let (Some(i1), Some(i2)) = (index1, index2) {
+            if i1 > i2 {
+                return false
+            }
+        }
+    }
+    return true;
+}
